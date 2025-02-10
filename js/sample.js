4f9dc9303ac7bddbdcaeeb60e81df83561ad4fc0
tndus3042 <sychoi1101@naver.com>
2025-02-10 16:13:36 +0900

    setTimeout, setInterval 실습

--- js/sample.js
+++ js/sample.js
@@ -1,4 +1,3 @@
-console.log('Hello Javascript!');
 //var num = 20 + 10000;
 // 한 줄 주석은 슬래시 2개로 표시
 
@@ -40,16 +39,58 @@ console.log('Hello Javascript!');
 //	console.log("Minor");
 //}
 
-let temperature = 20;
+//let temperature = 20;
+//
+//if (temperature >= 30) {
+//	console.log("정말 더운 날씨 입니다");
+//} else if (temperature >= 25) {
+//	console.log("더운 날씨 입니다.");
+//} else if (temperature >= 15){
+//	console.log("서늘한 날씨 입니다");
+//} else if (temperature >= 0){
+//	console.log("시원한 날씨 입니다");
+//} else {
+//	console.log("정말 추운 날씨입니다");
+//}
 
-if (temperature >= 30) {
-	console.log("정말 더운 날씨 입니다");
-} else if (temperature >= 25) {
-	console.log("더운 날씨 입니다.");
-} else if (temperature >= 15){
-	console.log("서늘한 날씨 입니다");
-} else if (temperature >= 0){
-	console.log("시원한 날씨 입니다");
-} else {
-	console.log("정말 추운 날씨입니다");
+
+// 3의 배수만 출력하기
+//let num = 0;
+//
+//while (num <= 60) {
+//    if (num % 3 == 0){
+//	    console.log(num);
+//	}
+//	num += 1
+//}
+
+
+// 윤년 계산기
+// 2000년부터 2030년까지의 윤년을 구하기
+// 연도가 4로 나누어 떨어지고 100으로 나누어 떨어지지않으면 윤년이다
+// 단, 연도가 400으로 나누어 떨어지면 윤년이다
+
+// 윤년의 조건:
+// - 4의 배수이면서 100의 배수가 아닌 해
+// - 또는 400의 배수인 해
+/*
+let year = 2000;
+
+while (year <= 2030){
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+        console.log(`${year}년 - 윤년입니다`);
+    }
+    year += 1;
+} */
+
+let year = 2000;
+
+while (year <= 2030){
+    if (year % 4 == 0 && year % 100 != 0) {
+        console.log(`${year}년 - 윤년입니다`);
+    } else if (year % 400 == 0) {
+	      console.log(`${year}년 - 윤년입니다.`);
+    }
+    year += 1;
 }
+
